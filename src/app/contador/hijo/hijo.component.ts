d7f3db041d4d4001055c6351d17d9862435ba854
Víctor hugo Morales Martínez <vchgmm_personal@outlook.com>
2021-11-30 18:32:23 -0600

    Terminando la Aplicación SIN NGRX

---
+++ src/app/contador/hijo/hijo.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styles: [
+  ]
+})
+export class HijoComponent implements OnInit {
+
+  @Input() contador!:number;
+  @Output() cambioContador = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  multiply(){
+    this.contador *= 2;
+    this.cambioContador.emit(this.contador);
+  }
+
+  divide(){
+    this.contador /= 2;
+    this.cambioContador.emit(this.contador);
+  }
+}
